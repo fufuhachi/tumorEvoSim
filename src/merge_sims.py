cb7057e12b8c4c6e0cdc1dbdc2e297cd507e3312
Thomas Rachman <trachman@andrew.cmu.edu>
2022-11-13 23:57:22 -0500

    merges time series

--- src/merge_sims.py
+++ src/merge_sims.py
@@ -3,61 +3,60 @@ import numpy as np
 import pandas as pd
 import sys
 import classes
-from main import *
 import os
 
 #functions
-def pkl_to_npz(path, param_names, params):
-    """Accepts path to .pkl file and simulation params that generated it. Returns pandas dataframe with 
-    columns of all information """
-    if path.split('.')[-1] !='pkl':
-        print('path must be to .pkl\nexiting...')
-        sys.exit()
-    tumor = classes.load_object(path)
-    df = tumor_summary(tumor, param_names, params)
-    df.to_csv(path.split('.')[-1]+'.npz')
-def tumor_summary(tumor, param_names, params):
+
+def tumor_summary(tumor, rep = 0):
     """turn tumor into dataframe with all the information: 
-        ncells x 5 matrix where columns are 'cell_ID' 'x','y' 'r' 'angle' 'genotype' 'n_drivers' 'drivers'"""
+        ncells x 5 matrix where columns are 'cell_ID' 'x','y' 'r' 'angle' 'genotype' 'n_drivers' 'drivers' """
     #turn list of cells into columns of information 
+    b = tumor.params['init_birth_rate']
+    dr_params = tumor.params['dr_params']
+    decay = 33
     mat = tumor.graph
     cell_ID = mat[mat > 0]
     x, y = np.indices(mat.shape)
-    x = x[mat > 0] - tumor.center[0]
-    y = y[mat > 0] - tumor.center[1]
+    x = x[mat >0] - tumor.center[0]
+    y = y[mat>0] - tumor.center[1]
     r = np.sqrt(x**2 + y**2)
     angle = (360/2/np.pi)*np.arctan2(y,x)
     
     genotype = np.array([tumor.cells.get_item(id).gen.ID for id in cell_ID], dtype = int)
     n_drivers = np.array([tumor.cells.get_item(id).gen.n_drivers for id in cell_ID], dtype = int)
-    drivers = np.array([tuple(tumor.cells.get_item(id).gen.drivers) for id in cell_ID],dtype = object)
-   
-    df = pd.DataFrame({'x':x,'y':y,'r':r,'angle':angle, 'genotype':genotype,'n_drivers': n_drivers, 'drivers':drivers})
-    for name, par in zip(param_names, params):
-        df[name] = par
+    drivers = [tuple(tumor.cells.get_item(id).gen.drivers) for id in cell_ID]
+    
+    death_rate = [tumor.cells.get_item(id).get_death_rate(**dr_params) for id in cell_ID]
+    
+    df = pd.DataFrame({'cell_ID' : cell_ID, 'x':x,'y':y,'r':r,'angle':angle, 
+    'genotype':genotype,'n_drivers': n_drivers, 'drivers':drivers, 'death_rate': death_rate})
+    df['cell_hge'] = df['death_rate']/(b - df['death_rate'] + decay)
+    df['rep'] = rep
     return df
 
-if __name__ == '__main__':
-    to_concat = []
-    folder_list = sys.argv[1]
-    catted_file = sys.argv[2]
-    for folder in np.loadtxt(folder_list, dtype = 'str'):
-        for i in range(100):
-            try:
-                tumor = classes.load_object(f'{folder}/rep={i}_ncells=5000.pkl').tumor
-            except(FileNotFoundError):
-                print(f'{folder}/rep={i}_ncells=5000.pkl not found')
-                continue
-            sf = folder.split('_')
-            param_names = (sf[0],sf[2],sf[4], 'rep')
-            params = (sf[1],sf[3],sf[5], i)
-            df = tumor_summary(tumor, param_names, params)
-            to_concat.append(df)
-            
-        print(f'{folder} done')
 
-    catted = pd.concat(objs = to_concat, ignore_index=True)
-    catted.to_csv(f'{catted_file}.npy')
+if __name__ == '__main__':
+    folder_list = sys.argv[1]
+    timepoints = []
+    for folder in np.loadtxt(folder_list, dtype = 'str'):
+        rep = folder.split('_')[-1]
+        
+        for file in os.listdir(folder):
+            if file.split('_')[-1].startswith('time'):
+                
+                sim = classes.load_object(os.path.join(dir,file))
+                summary = tumor_summary(sim.tumor)
+                summary['t'] = sim.tumor.t
+                timepoints.append(summary, rep = rep)
+        print(f'{folder} dataframe created')
+    print('concatenating all dataframes')
+    timedata = pd.concat(timepoints)
+    print('writing to file')
+    timedata.to_csv(f'experiment.csv')
+    print('done')
+
+    
+    
 
 
 
