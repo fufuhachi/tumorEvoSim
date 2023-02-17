56dd8744bbe0537fb9aa1eda5719ba1b71cf557c
Thomas Rachman <trachman@andrew.cmu.edu>
2023-02-16 22:21:43 -0500

    .

--- src/merge_sims2.py
+++ src/merge_sims2.py
@@ -11,25 +11,33 @@ import os
 import pandas as pd
 import numpy as np
 from multiprocessing import Pool, cpu_count
+from itertools import repeat
+
+def process_file(folder,file, rep):
+    if file.split('_')[-1].startswith('time'):
+        sim = classes.load_object(os.path.join(folder,file))
+        summary = simulation.tumor_summary(sim.tumor, rep = rep)
+        summary['t'] = sim.tumor.t
+        return summary
 
 def process_folder(folder):
     try:
         rep = int(folder.split('_')[-1])
-        for file in os.listdir(folder):
-            if file.split('_')[-1].startswith('time'):
-                sim = classes.load_object(os.path.join(folder,file))
-                summary = simulation.tumor_summary(sim.tumor, rep = rep)
-                summary['t'] = sim.tumor.t
-                return summary
+        files = os.listdir(folder)
+        summaries = []
+        with Pool() as pool:
+            summaries = pool.starmap(process_file, zip(repeat(folder),[(file, rep) for file in files]))
+        return [summary for summary in summaries if summary is not None]
     except(FileNotFoundError):
         print(f'{folder} not found. Trying next folder')
+
 
 if __name__ == '__main__':
     folder_list = sys.argv[1]
     try: 
         max_rep = int(sys.argv[2])
     except(IndexError):
-        max_rep = np.infty #change to desired number of replicates if less than total 
+        max_rep = len(folder_list.splitlines()) #change to desired number of replicates if less than total 
     print(f'max rep: {max_rep}')
 
     num_workers = int(sys.argv[3]) if len(sys.argv) > 3 else cpu_count()
