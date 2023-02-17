628425f7ade2ffa441081716283b5bafdaf338a7
Thomas Rachman <trachman@andrew.cmu.edu>
2023-02-17 10:08:41 -0500

    .

--- src/merge_sims2.py
+++ src/merge_sims2.py
@@ -20,7 +20,7 @@ def process_file(folder,file, rep):
         summary['t'] = sim.tumor.t
         return summary
 
-def process_folder(folder):
+def process_folder1(folder):
     print(f'processing {folder}')
     try:
         rep = int(folder.split('_')[-1])
@@ -30,6 +30,16 @@ def process_folder(folder):
     except(FileNotFoundError):
         print(f'{folder} not found. Trying next folder')
 
+def process_folder2(folder, n_worker):
+    try:
+        rep = int(folder.split('_')[-1])
+        files = os.listdir(folder)
+        summaries = []
+        with Pool(n_worker) as pool:
+            summaries = pool.starmap(process_file, [(file, rep) for file in files])
+        return pd.concat([summary for summary in summaries if summary is not None])
+    except(FileNotFoundError):
+        print(f'{folder} not found. Trying next folder')
 
 if __name__ == '__main__':
     folder_list = sys.argv[1]
@@ -49,9 +59,9 @@ if __name__ == '__main__':
         folders = folders[:max_rep] # limit to max_rep number of folders
     print(f'processing {len(folders)} folders')
     
-    with Pool(num_workers) as pool:
-        dataframes = pool.map(process_folder, folders)
-        dataframes = [df for df in dataframes if df is not None] # remove None values
+    #with Pool(num_workers) as pool:
+    dataframes = map(process_folder2, zip(folders,repeat(num_workers)))#pool.map(process_folder, folders)
+    dataframes = [df for df in dataframes if df is not None] # remove None values
     
     print('concatenating all dataframes')
     timedata = pd.concat(dataframes)
